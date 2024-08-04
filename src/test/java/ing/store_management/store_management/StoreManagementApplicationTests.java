1bdc16f292d95d2417a016cb998a1f29a9408429
dianaaadumitru <dianad2601@gmail.com>
2024-08-04 18:20:51 +0300

    create project

---
+++ src/test/java/ing/store_management/store_management/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ing.store_management.store_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
